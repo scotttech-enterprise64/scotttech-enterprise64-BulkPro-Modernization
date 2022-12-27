b1ae017502bb62f1ff900882ccd94df69779ced0
Shan <shan.asghar@enterprise64.com>
2022-12-27 17:14:02 +0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: "http://40.77.0.219:8042/api",
+  apiUrl: "http://40.77.0.219:8041/api",
   getDeviceInfo: "/device/DeviceAPI",
   login: "/login/LoginAPI"
 };
