689ed2a2078851b15fb26184c71f50c599f6257b
ovaise64 <ovais.shaikh@enterprise64.com>
2023-01-03 11:00:28 +0500

    icon fixes
    icon fixes on device app, bulkpro main screen

--- src/app/services/globalFunctions/global-functions.service.ts
+++ src/app/services/globalFunctions/global-functions.service.ts
@@ -50,24 +50,24 @@ export class GlobalFunctionsService {
     return [
       // Device Apps
       { name: 'BulkPro', icon: 'settings_cell' },
-      { name: 'Config', icon: 'summarize' },
+      { name: 'Config', icon: 'room_preferences' },
       { name: 'Consolidation', icon: 'fact_check' },
       { name: 'DeviceConfig', icon: 'book_online' },
-      { name: 'Exit', icon: 'power_settings_new' },
+      { name: 'Exit', icon: 'exit_to_app' },
       // Bulk Pro
       { name: 'Cycle Count', icon: 'low_priority', route: '/bulkpro/cycle-count' },
       { name: 'Pick', icon: 'archive', route: '/cyclecount' },
       { name: 'Case Pick', icon: 'medical_services', route: '/cyclecount' },
       { name: 'Put Away', icon: 'unarchive', route: '/cyclecount' },
-      { name: 'Receive', icon: 'pallet', route: '/cyclecount' },
-      { name: 'Hot Pick', icon: 'quickreply', route: '/cyclecount' },
-      { name: 'Hot Put Away', icon: 'outbox', route: '/cyclecount' },
-      { name: 'Hot Move', icon: 'roundabout_left', route: '/cyclecount' },
-      { name: 'Item Location', icon: 'assistant_navigation', route: '/cyclecount' },
-      { name: 'Location Status', icon: 'detector_status', route: '/cyclecount' },
+      { name: 'Receive', icon: 'markunread_mailbox', route: '/cyclecount' },
+      { name: 'Hot Pick', icon: 'outbox', route: '/cyclecount' },
+      { name: 'Hot Put Away', icon: 'move_to_inbox', route: '/cyclecount' },
+      { name: 'Hot Move', icon: 'control_camera', route: '/cyclecount' },
+      { name: 'Item Location', icon: 'explore', route: '/cyclecount' },
+      { name: 'Location Status', icon: 'not_listed_location', route: '/cyclecount' },
       { name: 'Location Move', icon: 'add_location_alt', route: '/cyclecount' },
       { name: 'About', icon: 'info', route: '/cyclecount' },
-      { name: 'Back', icon: 'power_settings_new', route: '/cyclecount' }
+      { name: 'Back', icon: 'exit_to_app', route: '/cyclecount' }
       // Config
       // Consolidation
       // Device Config
