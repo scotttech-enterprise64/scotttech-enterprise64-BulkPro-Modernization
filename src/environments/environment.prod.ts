170b3ccc4b5f26639e66566b8dc4ea7ddbe50c73
fasihhaidere64 <116360576+fasihhaidere64@users.noreply.github.com>
2023-01-03 16:05:53 +0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: "http://bulkpro-dev.enterprise64.net:8043/api",
+  apiUrl: "http://40.77.0.219:8043/api",
   getDeviceInfo: "/device/DeviceAPI",
   login: "/login/LoginAPI",
   count: "/count/CountAPI"
