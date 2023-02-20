7907187c62392685f53f14f00e11b7c1ec237754
affanyounus-e64 <113903931+affanyounus-e64@users.noreply.github.com>
2023-02-20 15:15:02 +0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: "http://40.77.0.219:8041/api",
+  apiUrl: "https://staging-e64.com:8041/api",
   getDeviceInfo: "/device/DeviceAPI",
   login: "/login/LoginAPI",
   count: "/count/CountAPI",
